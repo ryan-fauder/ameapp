8a0e9f16cda56d4a32a1f9aa69c40af0ec598df9
unknown <flaviomanuel258@gmail.com>
2020-05-04 19:42:11 -0300

    first version

---
+++ src/class/user.js
@@ -0,0 +1,39 @@
+const user = require('database/user.json');
+
+
+class User {
+
+    constructor(user){
+        this.user = user
+
+    }
+    storeData = async (user_inf) => {
+        try {
+          await AsyncStorage.setItem('user', JSON.stringify(user_inf));
+        } catch (error) {
+         console.log(error);
+        }
+      };
+
+      getMyValue = async () => {
+        try {
+          const value = await AsyncStorage.getItem('user')
+        } catch(error) {
+         console.log(error);
+         
+        }      
+      }
+
+      removeValue = async () => {
+        try {
+          await AsyncStorage.removeItem('user')
+        } catch(e) {
+            console.log(error)
+        }
+      
+        
+      }
+
+      
+
+}
---
+++ src/database/user.js
@@ -0,0 +1,10 @@
+const "user": {
+    "id": "1",
+    "name": "Ryan",
+    "email": "ryan123@gmail.com",
+    "senha": "123456",
+    "cep": 77777,
+    "moeda": 54,
+    "xp": 120,
+    "level": 2
+}
